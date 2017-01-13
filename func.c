6e2199cb4b08bbe2cf2ab5d76da927c17e580cae
mugi111 <takumi5349pino9355@gmail.com>
2017-01-14 00:13:06 +0900

    主要関数の枠組みができた

---
+++ func.c
@@ -0,0 +1,104 @@
+#include "stm32f0xx_gpio.h"
+
+
+void generalInit()
+{
+	GPIO_InitTypeDef SW_Output;
+	SW_Output.GPIO_Pin = GPIO_Pin_0 || GPIO_Pin_1 || GPIO_Pin_2 || GPIO_Pin_3;
+	SW_Output.GPIO_Mode = GPIO_Mode_OUT;
+	SW_Output.GPIO_PuPd = GPIO_PuPd_UP;
+	SW_Output.GPIO_Speed = GPIO_Speed_Level_1;
+//	SW_Output.GPIO_OType =
+	GPIO_Init(GPIOA, &SW_Output);
+
+	GPIO_InitTypeDef SW_Input;
+	SW_Input.GPIO_Pin = GPIO_Pin_4 || GPIO_Pin_5 || GPIO_Pin_6;
+	SW_Input.GPIO_Mode = GPIO_Mode_IN;
+	SW_Input.GPIO_PuPd = GPIO_PuPd_UP;
+	SW_Input.GPIO_Speed = GPIO_Speed_Level_1;
+	GPIO_Init(GPIOA, &SW_Input);
+
+	GPIO_InitTypeDef DisplayLED;
+	DisplayLED.GPIO_Pin = GPIO_Pin_7 || GPIO_Pin_8 || GPIO_Pin_9 || GPIO_Pin_10 || GPIO_Pin_11;
+	DisplayLED.GPIO_Mode = GPIO_Mode_OUT;
+	DisplayLED.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	DisplayLED.GPIO_Speed = GPIO_Speed_Level_1;
+	GPIO_Init(GPIOA, &DisplayLED);
+
+	TIM_TimeBaseInitTypeDef StatusCount;
+//	StatusCount.TIM_ClockDivision =
+//	StatusCount.TIM_CounterMode =
+//	StatusCount.TIM_Period =
+//	StatusCount.TIM_Prescaler =
+	TIM_TimeBaseStructInit(&StatusCount);
+
+}
+
+int readSW(int status)
+{
+	int SWnum = -1;
+	switch (status) {
+		case 0:
+			if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4)){
+				SWnum = 1;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5)){
+				SWnum = 2;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6)){
+				SWnum = 3;
+			}
+			break;
+
+		case 1:
+			if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4)){
+				SWnum = 4;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5)){
+				SWnum = 5;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6)){
+				SWnum = 6;
+			}
+			break;
+
+		case 2:
+			if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4)){
+				SWnum = 7;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5)){
+				SWnum = 8;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6)){
+				SWnum = 9;
+			}
+			break;
+
+		case 3:
+			if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4)){
+				SWnum = 11;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5)){
+				SWnum = 0;
+			}else if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6)){
+				SWnum = 12;
+			}
+			break;
+
+		default:
+			break;
+	}
+	return SWnum;
+}
+
+void judgeNum(int SWnum)
+{
+
+}
+
+int checkPass(int input[])
+{
+	if((pass[0]==input[0])&&(pass[1]==input[1])&&(pass[2]==input[2])&&(pass[3]==input[3])){
+		return 1;
+	}else {
+		return 0;
+	}
+}
+
+void DriveMotor(int dig)
+{
+
+}
---
+++ func.h
@@ -0,0 +1,9 @@
+
+int pass[4] = {0, 0, 0, 0};
+int flagSharp;
+
+void generalInit(void);
+int readSW(int status);
+void judgeNum(int SWnum);
+int checkPass(int input[]);
+void DriveMotor(int dig);
