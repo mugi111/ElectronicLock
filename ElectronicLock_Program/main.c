d4156510541805e40f168868adff4d14303f914f
mugi111 <takumi5349pino9355@gmail.com>
2017-02-26 19:25:03 +0900

    不要な部分の削除

--- ElectronicLock_Program/main.c
+++ ElectronicLock_Program/main.c
@@ -10,30 +10,17 @@ int column;
 
 int main(void)
 {
-	int i, j=0, k, l=0;
+	int i, j=0;
 	int flag[3];
     int keypad[12] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
     int keypad_prev[12] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
     SW_GPIO_Init();
 	TIM3_Init(4800, 1000);
 	TIM3CH1_PWMConfig(1200);
-	TIM14_Init(480, 100);
-
-	NVIC_EnableIRQ(TIM14_IRQn);
 
     while(1)
     {
-//    	for (i = 0; i < 10000; i++) {
-//
-//    	}
 RETURN:
-//		column++;
-//
-//		if(column==3){
-//			column = 0;
-//		}
-
-
 		for(i=0;i<3;i++){
 			Chenge_Output(i);
 			switch(i){
@@ -90,10 +77,8 @@ RETURN:
 						for (j=0; j<3; j++)	input[j] = input[j+1];
 						input[3] = i;
 					}else if(i == 10){
-						NVIC_EnableIRQ(TIM14_IRQn);
 						GPIO_WriteBit(GPIOA, GPIO_Pin_5, 0);
 					}else if(i == 11){
-						NVIC_DisableIRQ(TIM14_IRQn);
 						GPIO_WriteBit(GPIOA, GPIO_Pin_5, 0);
 						GPIO_WriteBit(GPIOA, GPIO_Pin_8, 1);
 						GPIO_WriteBit(GPIOA, GPIO_Pin_9, 0);
@@ -177,18 +162,12 @@ RETURN:
 WAIT:
 		if(GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_7)){
 			for (i = 0; i < 12; ++i)	keypad_prev[i] = -1;
-			NVIC_EnableIRQ(TIM14_IRQn);
 			goto RETURN;
 		}
     }
 }
 
-void TIM14_IRQHandler(void)
-{
-	int i;
 
-	TIM_ClearITPendingBit(TIM14,  TIM_IT_Update);
-}
 
 void EXTI4_IRQHandler(void)
 {
